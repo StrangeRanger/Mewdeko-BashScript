adc02a931b9f3b690fd3c76c67dc42a71557ba6d
Hunter T <strangeranger01@randomserver.xyz>
2022-09-06 19:16:58 -0700

    Revert ability to update existing config variables

--- installer_prep.sh
+++ installer_prep.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Acts as a transition script from linuxAIO revision 2 to 3. During these changes, many
+# Acts as a transition script from linuxAIO revision 2 to 4. During these changes, many
 # of the files were renamed and had their file extension ('.sh') removed.
 #
 ########################################################################################
@@ -32,13 +32,6 @@ case $(uname -m) in
     armv*)  bits="32"; _ARCH="?" ;;
     *)      bits="?";  _ARCH="?" ;;
 esac
-
-## Save the values of the current Configuration Variables specified in 'linuxAIO.sh', to
-## be set in 'linuxAIO'.
-installer_branch=$(grep '^installer_branch=.*' linuxAIO.sh)
-installer_branch_found="$?"
-bot_install_version=$(grep '^export _MEWDEKO_INSTALL_VERSION=.*' linuxAIO.sh)
-bot_install_version_found="$?"
 
 
 #### End of [ Variables ]
@@ -109,28 +102,22 @@ fi
 #### [[ Update Files ]]
 
 
+if   [[ -f linuxAIO.sh ]]; then
+    echo "Backing up 'linuxAIO.sh' as 'linuxAIO.sh.old'..."
+    mv linuxAIO.sh linuxAIO.sh.bak
+elif [[ -f linuxAIO ]]; then
+    echo "Backing up 'linuxAIO' as 'linuxAIO.old'..."
+    mv linuxAIO linuxAIO.bak
+fi
+
+echo "Downloading latest version of 'linuxAIO'..."
 curl -O "$_RAW_URL"/linuxAIO && sudo chmod +x linuxAIO
-echo "Applying existing configurations to 'linuxAIO'..."
-
-## Set $installer_branch inside of 'linuxAIO'.
-[[ $installer_branch_found = 0 ]] \
-    && sed -i "s/^installer_branch=.*/$installer_branch/" linuxAIO
-
-## Set $bot_install_version inside of 'linuxAIO'.
-[[ $bot_install_version_found = 0 ]] \
-    && sed -i "s/^export _MEWDEKO_INSTALL_VERSION=.*/$bot_install_version/" linuxAIO
+echo "${_CYAN}NOT applying existing configurations to the new 'linuxAIO'${_NC}"
 
 echo "Cleaning up..."
 [[ -f MewdekoRun.sh ]]     && mv MewdekoRun.sh MewdekoRun
 [[ -f installer-prep ]]    && rm installer-prep
 [[ -f installer_prep.sh ]] && rm installer_prep.sh
-
-if [[ -f linuxAIO.sh && -f linuxAIO ]]; then
-    rm linuxAIO.sh
-else
-    echo "${red}'linuxAIO.sh' and 'linuxAIO' should exist, but one or both do not.${nc}"
-    exit 1
-fi
 
 
 #### End of [[ Update Files ]]
